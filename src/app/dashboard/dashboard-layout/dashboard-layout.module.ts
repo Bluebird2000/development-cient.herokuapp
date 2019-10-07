bf02d58bd1195de2f984b86eeb4376854413aef1
kennymorgan1 <kenneth.ezeugwu@photizzo.com>
2019-10-07 16:22:42 +0100

    generate sidebar component

--- src/app/dashboard/dashboard-layout/dashboard-layout.module.ts
+++ src/app/dashboard/dashboard-layout/dashboard-layout.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { DashboardLayoutRoutingModule } from './dashboard-layout-routing.module';
 import { DashboardLayoutComponent } from './dashboard-layout.component';
 import { AdminComponent } from './pages/admin/admin.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
-  declarations: [DashboardLayoutComponent, AdminComponent],
+  declarations: [DashboardLayoutComponent, AdminComponent, SidebarComponent],
   imports: [
     CommonModule,
     DashboardLayoutRoutingModule
