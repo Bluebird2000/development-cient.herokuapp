4bba74b61dbd84711a2a13ccb02b46f3f5f8e2e8
kennymorgan1 <kenneth.ezeugwu@photizzo.com>
2019-10-10 23:52:05 +0100

    generate nav bar component

--- src/app/dashboard/dashboard-layout/components/components.module.ts
+++ src/app/dashboard/dashboard-layout/components/components.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
