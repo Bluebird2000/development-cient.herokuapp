85c13694c496c70c7539450a2e39b0c7638f89ea
kennymorgan1 <kenneth.ezeugwu@photizzo.com>
2019-10-17 05:17:29 +0100

    design add category form

--- src/app/dashboard/dashboard-layout/pages/category/category.component.ts
+++ src/app/dashboard/dashboard-layout/pages/category/category.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from '../../../../service/main-service.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+  categories;
+  constructor(private service: MainService) { }
 
   ngOnInit() {
+  }
+
+  listCategories() {
+    this.service.listCategories().subscribe((data: any) => {
+      if (data) {
+        this.categories = data.data;
+      }
+    });
   }
 
 }
