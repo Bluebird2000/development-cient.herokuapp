50940de798e3f411bd489e207e7540a5b8557233
kennymorgan1 <kenneth.ezeugwu@photizzo.com>
2019-10-19 11:39:06 +0100

    list all forums

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PageTitleService } from '../core/page-title/page-title.service';
 import { ChkService } from '../service/chk.service';
+import { MainService } from '../service/main-service.service';
 
 @Component({
   selector: 'angly-about',
@@ -10,39 +11,42 @@ import { ChkService } from '../service/chk.service';
 export class ForumComponent implements OnInit {
 
    /* Variables */
-   services : any;
-   about    : any;
-   team     : any;
-   contact  : any;
+   services: any;
+   about: any;
+   team: any;
+   contact: any;
+   forums;
+   setTimeProgress;
+   fetchForumProgress = 10;
 
-   constructor(private pageTitleService: PageTitleService, private service:ChkService) {
+   constructor(private pageTitleService: PageTitleService, private service: ChkService, private mainSrv: MainService) {
 
       /* Page title */
-      this.pageTitleService.setTitle(" Know More About Us");
+      this.pageTitleService.setTitle(' Know More About Us');
 
       /* Page subTitle */
-      this.pageTitleService.setSubTitle(" Our latest news and learning articles. ");
+      this.pageTitleService.setSubTitle(' Our latest news and learning articles. ');
 
       this.service.getServices().
-         subscribe(response => {this.services = response},
+         subscribe(response => {this.services = response; },
                   err       => console.log(err),
                   ()        =>this.services
                );
 
       this.service.getAbout().
-         subscribe(response => {this.about = response},
+         subscribe(response => {this.about = response; },
                    err      => console.log(err),
                    ()       => this.about
                );
 
       this.service.getTeam().
-         subscribe(response => {this.team = response},
+         subscribe(response => {this.team = response; },
                    err      => console.log(err),
                    ()       => this.team
                );
 
       this.service.getContactContent().
-         subscribe(response => {this.contact = response},
+         subscribe(response => {this.contact = response; },
                    err      => console.log(err),
                    ()       => this.contact
                );
@@ -51,20 +55,41 @@ export class ForumComponent implements OnInit {
 
 
    ngOnInit() {
+      this.viewForums();
    }
-   
+
+   viewForums() {
+      this.fetchingForumProgress();
+      this.mainSrv.listForums().subscribe((data: any) => {
+        if (data) {
+           console.log(data);
+          this.forums = data.data;
+          clearInterval(this.setTimeProgress);
+        }
+      });
+    }
+
+    fetchingForumProgress() {
+      this.setTimeProgress = setInterval(() => {
+        if (this.fetchForumProgress < 90) {
+          this.fetchForumProgress += 10;
+        }
+      }, 1000);
+    }
+
    /*
    * Social links
    */
-  socialDetails : any = [
+  // tslint:disable-next-line: member-ordering
+  socialDetails: any = [
     { url: 'https://www.facebook.com/', icon : 'fa-facebook'},
     { url: '', icon : 'fa-twitter text-info'},
     { url: '', icon : 'fa-pinterest text-danger'},
-  ]
+  ];
 
   /*
    * Classes of social ul, li
    */
-  socialsClasses : any = {ulClass:"", liClass:"", linkClass:""}
+  socialsClasses: any = {ulClass:'', liClass:"", linkClass:""}
 
 }
